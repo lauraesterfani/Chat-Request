478b166e6376392453d484dbb9f1ae1f55897603
Victor Gustavo <victorgustavo.dev@gmail.com>
2025-08-17 21:30:13 -0300

    fix: corrige e identifica rotas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\AdminAuthController;
-
-// Namespaces dos seus controllers
 use App\Http\Controllers\AlunoController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CampusController;
@@ -14,29 +12,38 @@ use App\Http\Controllers\TipoRequerimentoController;
 use Illuminate\Support\Facades\Route;
 
 // --- ROTAS PÚBLICAS DE AUTENTICAÇÃO ---
-// Acessíveis sem token
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/login-adm', [AdminAuthController::class, 'loginWithEmail']);
-// Route::post('/login-adm', [AdminAuthController::class, 'login']);
-Route::get('/requerimentos', [RequerimentoController::class, 'index']);
-Route::post('/requerimentos', [RequerimentoController::class, 'store']);
-Route::get('/alunos/{aluno}/requerimentos', [RequerimentoController::class, 'listByAluno']);
-Route::get('/my-requerimentos', [RequerimentoController::class, 'myRequerimentos']);
-// --- ROTAS PROTEGIDAS PARA ALUNOS (exigem token de aluno) ---
+Route::post('/validate-token', [AuthController::class, 'validateToken']);
+
+
+// --- ROTAS PRIVADA COM AUTENTICAÇÃO NECESSÁRIA ---
 Route::middleware('auth:api')->group(function () {
+
+    //Matriculas
     Route::get('/requerimentos/{id}', [RequerimentoController::class, 'show']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/my-requerimentos', [RequerimentoController::class, 'myRequerimentos']);
+    Route::get('/requerimentos', [RequerimentoController::class, 'index']);
+    Route::post('/requerimentos', [RequerimentoController::class, 'store']);
     Route::patch('/requerimentos/{requerimento}/status', [RequerimentoController::class, 'updateStatus']);
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/validate-token', [AuthController::class, 'validateToken']);
-    Route::get('/my-registrations', [MatriculaController::class, 'myRegistrations']);
+
+    //Requerimentos
+    Route::get('/alunos/{aluno}/requerimentos', [RequerimentoController::class, 'listByAluno']);
     Route::apiResource('alunos', AlunoController::class);
+
+    //Rotas de autenticação
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    //Rotas de administração
+    Route::get('/my-registrations', [MatriculaController::class, 'myRegistrations']);
     Route::apiResource('matriculas', MatriculaController::class);
+
+    // Demais recursos
     Route::apiResource('cursos', CursoController::class);
     Route::apiResource('campus', CampusController::class);
     Route::apiResource('tipos-anexo', TipoAnexoController::class);
     Route::apiResource('tipos-requerimento', TipoRequerimentoController::class);
 });
 
-// Route::apiResource('requerimentos', RequerimentoController::class);
